7fc634472b354b7870ef94cdbc5d877dc95d2a24
Baboucarr Sanneh <basanneh@gmail.com>
2020-08-06 00:20:18 -0400

    made changes to the code of DFS_BFS.py

--- DFS_BFS.py
+++ DFS_BFS.py
@@ -1,18 +1,4 @@
-# DFS algorithm in Python
-
-
-# DFS algorithm
-def dfs(graph, start, visited=None):
-    if visited is None:
-        visited = set()
-    visited.add(start)
-
-    print(start)
-
-    for next in graph[start]:
-        if next not in visited:
-            dfs(graph, next, visited)
-    return visited
+#BFS and DFS algorithm in Python
 
 
 
@@ -27,11 +13,25 @@ def bfs(graph, start):
         output = queue.pop(0)
         print(output, end=" ")
         for i in graph[output]:
-
             if i not in visited:
                 queue.append(i)
                 visited.add(i)
             
+
+
+# DFS algorithm
+def dfs(graph, start, visited=None):
+    if start is None:
+        return
+    if visited is None:
+        visited = set()
+
+    visited.add(start)
+    print(start, end=' ')
+    for n in graph[start]:
+        if n not in visited:
+            dfs(graph, n, visited)
+
 
 graph = {'A': set(['B', 'C']),
          'B': set(['A', 'D', 'E']),
@@ -42,6 +42,7 @@ graph = {'A': set(['B', 'C']),
 
 print('DFS:')
 dfs(graph, 'C')
-print('BFS: ')
+
+print('\nBFS: ')
 bfs(graph, 'A')
 print()
